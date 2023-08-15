9751c39e198c8763ba2f9f79a999e63f9e6d247d
shivam-jainn <shivam.clgstash@gmail.com>
2023-08-15 09:23:41 +0530

    updated to try pulling on ec2

--- server/index.js
+++ server/index.js
@@ -16,5 +16,5 @@ app.use("/user", userRouter)
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
 mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
-
+console.log("update");
 app.listen(3000, () => console.log('Server running on port 3000'));
---
+++ server/note.md
@@ -0,0 +1,3 @@
+http : 80
+https : 443 
+ssh default port : 22
